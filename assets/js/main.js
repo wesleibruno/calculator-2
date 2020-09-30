719aa0509d38a1d79707da5c8eae7bff8a2a7af6
Weslei Bruno <weslei945@gmail.com>
2020-09-30 13:45:48 -0300

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
 function Calculadora() {
+    this.display = document.querySelector('.display');
+    
+    this.capturaCliques = () => {
+        document.addEventListener('click', event => {
+            const el = event.target;
+            if(el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if(el.classList.contains('btn-clear')) this.clear(el);
+            if(el.classList.contains('btn-del')) this.dell(el);
+            if(el.classList.contains('btn-eq')) this.RealizaConta(el);
+        });
+    };
 
+    this.addNumDisplay = el => this.display.value += el.innerText;
+
+    this.inicia = () => {
+        this.capturaCliques();
+    }
 }
 
 const calculadora = new Calculadora();
+calculadora.inicia();
