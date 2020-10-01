6e4da154355e0f38b4fd64237c5750bf8724aa38
Weslei Bruno <weslei945@gmail.com>
2020-10-01 10:41:11 -0300

    last commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,52 @@
 function Calculadora() {
     this.display = document.querySelector('.display');
     
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
+    };
+
+    this.capturaEnter = () => {
+        this.display.addEventListener('keypress', e => {
+            if(e.keyCode === 13) {
+            this.RealizaConta();
+            }
+        });
+    };
+
     this.capturaCliques = () => {
         document.addEventListener('click', event => {
             const el = event.target;
             if(el.classList.contains('btn-num')) this.addNumDisplay(el);
             if(el.classList.contains('btn-clear')) this.clear(el);
-            if(el.classList.contains('btn-del')) this.dell(el);
+            if(el.classList.contains('btn-del')) this.del(el);
             if(el.classList.contains('btn-eq')) this.RealizaConta(el);
         });
     };
 
-    this.addNumDisplay = el => this.display.value += el.innerText;
+    this.RealizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
 
-    this.inicia = () => {
-        this.capturaCliques();
+            if(!conta) {
+                alert('Conta inválida');
+                return;
+            }
+
+            this.display.value = conta;
+        }catch(e) {
+            alert('Conta inválida');
+            return;
+        }
     }
+
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText;
+        this.display.focus();
+    }
+    this.clear = () => this.display.value = '';
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+
 }
 
 const calculadora = new Calculadora();
